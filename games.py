b27609930a91fc1a9abe4fcdc210474059e21431
Pierre <ohlmann@irif.fr>
2020-12-26 19:05:49 +0100

    can now save and import games

--- games.py
+++ games.py
@@ -30,6 +30,35 @@ class parity_game:
             self.succ[t[0]].append((t[1],t[2]))
             self.pred[t[1]].append((t[0],t[2]))
             
+    def save_to_file(self, name):
+        file = open("instances/"+name, 'w')
+        file.write(self.tostring())
+        file.close()
+    
+    @classmethod
+    def fromfile(cls, filename):
+        file = open("instances/" + filename, 'r')
+        n = int(file.readline())
+        d = int(file.readline())
+        str_edges = file.readline()
+        str_player = file.readline()
+        player = list(map(int, str_player.split(", ")))
+        edges = str_edges.split("), (")
+        m = len(edges)
+        edges[0] = edges[0][1:]
+        edges[m-1] = edges[m-1][:-2]
+        for i in range(m):
+            edges[i] = tuple(map(int, edges[i].split(", ")))        
+        file.close()
+        return(parity_game(n, d, edges, player))
+        
+    def tostring(self):
+        return(str(self.size) + '\n'
+            + str(self.max_priority) + '\n'
+            + str(self.edges)[1:-1] + '\n'
+            + str(self.player)[1:-1]
+        )
+        
     def print(self):
         print("PARITY GAME:")
         print("game size: ", self.size)
